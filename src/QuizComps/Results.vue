d00ccd7fe55a355a63f3784f5203647ee38ce166
Christopher-Masselli <christophermasselli@gmail.com>
2019-11-09 14:32:28 -0500

    Results complete

--- src/QuizComps/Results.vue
+++ src/QuizComps/Results.vue
@@ -1,5 +1,19 @@
 <template>
-<p>test</p>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">Correct answers</th>
+        <th scope="col">Your submitted answers</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="x in ans" :class="pickClass(x[0], x[1] )">
+        
+          <th scope="row">{{x[0]}}</th>
+          <td>{{x[1]}}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 <script>
 export default {
@@ -15,7 +29,22 @@ export default {
     }
   },
   data: () => ({
-
-  })
+      ans: null
+  }),
+  created() {
+    this.ans = this.correctAns.map( (x, i) => {
+      return [x, this.userAns[i]]
+    })
+    console.log(this.ans)
+  },
+  methods: {
+    pickClass(x, y){
+      if (x == y)
+      {
+        return 'table-success'
+      }
+      return 'table-danger'
+    }
+  }
 };
 </script>
